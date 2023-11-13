eea0cf7b42e43fc016ea07354d15c802702c4d6c
freshovie <egorkeovie@gmail.com>
2023-11-13 19:57:44 +0100

    Add cart

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ $('.owl-carousel').owlCarousel({
     margin:10,
     nav:true,
     dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: false,
     responsive:{
         0:{
             items:1
@@ -15,3 +18,35 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+  
