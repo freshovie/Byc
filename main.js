2ff7f1e3feedfeb35f79ae856253f4d711892ea8
freshovie <egorkeovie@gmail.com>
2023-11-16 09:00:42 +0100

    new files

--- main.js
+++ main.js
@@ -36,17 +36,43 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
+//   let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    // dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+//   dots[slideIndex-1].className += " active";
+  
 }
+
+function myFunction() {
+    let counterElement = document.getElementById('myNumber');
+    let currentValue = counterElement.textContent
+    counterElement.textContent =parseInt(currentValue)+ 1
+}
+
+const decrement = () => {
+    let counterElement = document.getElementById('myNumber');
+    let currentValue = counterElement.textContent
+    counterElement.textContent =parseInt(currentValue) -1
+   if(currentValue <= 0){
+    counterElement.textContent = 0
+   }
+  };
+//   function myFunction2() {
+//     let counterElement = document.getElementById('myNumber');
+//     let currentValue = parseInt(counterElement.textContent);
+
+//     if (currentValue > 0) {
+//       currentValue--;
+//       counterElement.textContent = currentValue;
+//     } else {
+//       console.log("Value reached 0");
+//     }
+  
   
